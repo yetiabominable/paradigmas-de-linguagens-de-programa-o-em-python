dc8d0d19467d30b1ecce706c5116655f03ef504d
Yeti <165808137+yetiabominable@users.noreply.github.com>
2024-04-02 15:18:47 +0000

    variables in python

---
+++ python-basico/variables.py
@@ -0,0 +1,19 @@
+a = "Sou uma variavel do tipo string"
+print(a)
+
+# Atribuição múltipla
+a, b = 1, 2
+print(a, b)
+
+# Troca de valores entre variáveis
+a, b = b, a
+print(a, b)
+
+# Operadores compostos
+x = 0 
+
+x += 2 # x = x + 2
+x -= 2 # x = x - 2
+x *= 2 # x = x * 2
+x /= 2 # x = x / 2
+x %= 2 # x = x % 2
