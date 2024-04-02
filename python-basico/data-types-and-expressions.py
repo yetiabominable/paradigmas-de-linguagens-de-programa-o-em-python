ae1a1801d187ef855b78c037422b776790ecfe7f
Yeti <165808137+yetiabominable@users.noreply.github.com>
2024-04-02 18:33:37 +0000

    data types and expressions in python

---
+++ python-basico/data-types-and-expressions.py
@@ -0,0 +1,39 @@
+# Tipo int
+print(type(1_000_000))
+
+# Tipo float
+print(type(50.0))
+
+x = 50
+y = 50,0
+
+print(x)
+print(y)
+
+print(type(2 + 3+ 1))
+print(type(2 + 3+ 1.0))
+
+print (2**3)
+print (type(2**3))
+print (type(2.0**3))
+
+x = 5 / 2
+print(x)
+
+# Tipo complex
+r = complex(2,5)
+print(r)
+
+# Tipo bool
+print(2 > 3)
+not(2 > 3)
+
+# String
+curso = 'Ensino a Distância'
+print(curso)
+curso.upper()
+print(curso)
+curso.lower()
+print(curso)
+curso.split()
+print(curso)
