076e97281f70ab529e9900a749e8b54f2eb9f4b9
Yeti <165808137+yetiabominable@users.noreply.github.com>
2024-04-02 22:44:15 +0000

    output data formmarte

---
+++ python-basico/formatacao-dados-saida.py
@@ -0,0 +1,20 @@
+hora = 10
+minutos = 26
+segundos = 18 
+
+print(str(10) + ":" + str(minutos) + ":" + str(segundos))
+print("{} : {} : {}" .format(hora, minutos, segundos))
+
+print("{:4}, {:5}" .format(10, 100))
+
+print("{:8.5}" .format(10 / 3))
+
+seq = [0, 1, 2]
+print(seq)
+
+str = "Hello World"
+print(str[0:4])
+print(str[2:8])
+
+print(str[::-1])
+print(str[8:2:-1])
